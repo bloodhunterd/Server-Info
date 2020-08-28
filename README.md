cb7d7d983409a24ee2e5a35a68f44cc88d83705d
BloodhunterD <bloodhunterd@bloodhunterd.com>
2020-08-28 13:44:55 +0200

    Fixes #1 No IP address on Ubuntu. Fixes #2 No CPU core speed on Raspberry Pi. Adjustable by configuration system name and date format added.

--- README.md
+++ README.md
@@ -5,16 +5,33 @@
 
 Simple script for dynamic system information displayed as message of the day banner on login.
 
+## Deployment
+
+### Download
+
+Download the script and configuration file.
+
+[![Motd script](https://img.shields.io/github/size/bloodhunterd/motd/motd.system-info.sh?label=Motd%20script&style=for-the-badge)](https://github.com/bloodhunterd/motd/blob/master/motd.system-info.sh)
+[![Motd configuration](https://img.shields.io/github/size/bloodhunterd/motd/motd.system-info.conf?label=Motd%20script&style=for-the-badge)](https://github.com/bloodhunterd/motd/blob/master/motd.system-info.conf)
+
 ### Installation
 
-Put the script into **/etc/profile.d/** and make it executeable.
+Put the script and configuration file into **/etc/profile.d/** and make the script file executeable.
 
 ```
-mv ./motd.system-info.sh /etc/profile.d
+mv ./motd.system-info.* /etc/profile.d
 chmod +x /etc/profile.d/motd.system-info.sh
 ```
 
-### Update
+### Configuration
+
+| Setting | Values | Default | Description
+|--- |--- |--- |---
+| SYSTEM_NAME | Any valid system name | Hostname | The name of the system.
+| DATE_FORMAT | [date - Linux man page](https://linux.die.net/man/1/date) | %x %X | Format of displayed date.
+| INTERFACE | Any valid interface name | eth0 | The interface to show the IP address of.
+
+## Update
 
 Please note the [changelog](https://github.com/bloodhunterd/motd/blob/master/CHANGELOG.md) to check for configuration changes before updating.
 
@@ -23,4 +40,5 @@ Please note the [changelog](https://github.com/bloodhunterd/motd/blob/master/CHA
 * [BloodhunterD](https://github.com/bloodhunterd)
 
 ## License
+
 This project is licensed under the MIT - see [LICENSE.md](https://github.com/bloodhunterd/motd/blob/master/LICENSE) file for details.
--- motd.system-info.sh
+++ motd.system-info.sh
@@ -1,16 +1,43 @@
 #!/bin/bash
 
-DATE=`date`
+# Get path to script directory
+if [ -n "${BASH_SOURCE[0]}" ]; then
+  DIR=$(dirname "${BASH_SOURCE[0]}")
+elif [ -n "${0}" ]; then
+  DIR=$(dirname "$(readlink -f "$0")")
+fi
 
-SYSTEM_NAME=`uname -snrm`
-SYSTEM_UPTIME=`uptime | awk '{print $3 " " $4}' | sed s'/.$//'`
+# Include config
+. "${DIR}/motd.system-info.conf"
+
+# Fallbacks
+if [ -z ${SYSTEM_NAME+x} ]; then
+    SYSTEM_NAME=`hostname`
+fi
+if [ -z ${DATE_FORMAT+x} ]; then
+    DATE_FORMAT="%x %X"
+fi
+if [ -z ${INTERFACE+x} ]; then
+    INTERFACE="eth0"
+fi
+
+DATE=`date +"${DATE_FORMAT}"`
+TIMEZONE=`timedatectl | grep "Time" | awk '{print $3" "$4" UTC"$5}'`
+
+DISTRIBUTION_NAME=`lsb_release -si`
+DISTRIBUTION_VERSION=`lsb_release -sr`
+DISTRIBUTION_CODENAME=`lsb_release -sc`
+
+UPTIME=`uptime | awk '{print $3 " " $4}' | sed s'/.$//'`
 
 PROCESSES_RUNNING=`ps aux | wc -l`
 
-CPU_CORES=`nproc`
+CPU_MODEL=`cat /proc/cpuinfo | grep -m 1 "model name" | awk '{a="";for (i=4;i<=NF;i++){a=a$i" "}print a}'`
 CPU_LOAD=`cat /proc/loadavg | awk '{print $1*100}'`
+CPU_LOAD_AVG=`cat /proc/loadavg | awk '{print $1" "$2" "$3}'`
+CPU_CORES=`nproc`
 CPU_USAGE=$((CPU_LOAD / CPU_CORES))
-CPU_MHZ=`cat /proc/cpuinfo | grep -m 1 "cpu MHz" | awk '{print $4}'`
+CPU_SPEED=`lscpu | grep -m 1 "MHz" | awk '{for(i=NF;i>=1;i--) printf "%s ", $i;print ""}' | awk '{print $1}' | cut -f1 -d"."`
 
 MEMORY_TOTAL=`free -m | grep "Mem" | awk '{print $2}'`
 MEMORY_USAGE=`free -m | grep "Mem" | awk '{print $3}'`
@@ -25,29 +52,29 @@ DISK_TOTAL=`df -h --total | grep "total" | awk '{print $2}'`
 DISK_USAGE=`df -h --total | grep "total" | awk '{print $3}'`
 DISK_USAGE_PERCENT=`df -h --total | grep "total" | awk '{print $5}'`
 
-IP_ADDRESS=`ip addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`
+IP_ADDRESS=`ip addr show ${INTERFACE} | grep -oP '(?<=inet\s)\d+(\.\d+){3}'`
 
 COLOR_DEFAULT="\033[0m"
 COLOR_INFO="\033[1;34m"
 COLOR_VALUE="\033[1;32m"
+COLOR_HEAD="\033[1;33m"
+COLOR_HEADLINE="\033[1;31m"
 
 printf "\n"
-printf " ${COLOR_INFO}${SYSTEM_NAME}\n"
-printf " ${COLOR_DEFAULT}"
-printf "==============================================\n"
-printf " ${COLOR_INFO}DATE          |${COLOR_VALUE} %s\n" "${DATE}"
-printf " ${COLOR_INFO}SYSTEM UPTIME |${COLOR_VALUE} %s\n" "${SYSTEM_UPTIME}"
-printf " ${COLOR_DEFAULT}"
-printf "==============================================\n"
-printf " ${COLOR_INFO}PROCESSES     |${COLOR_VALUE} %s\n" "${PROCESSES_RUNNING} (running)"
-printf " ${COLOR_DEFAULT}"
-printf "==============================================\n"
-printf " ${COLOR_INFO}CPU USAGE     |${COLOR_VALUE} %s\n" "${CPU_USAGE}% (${CPU_CORES} cores at ${CPU_MHZ} MHz)"
-printf " ${COLOR_INFO}MEMORY USAGE  |${COLOR_VALUE} %s\n" "${MEMORY_USAGE_PERCENT}% (${MEMORY_USAGE} MB of ${MEMORY_TOTAL} MB)"
-printf " ${COLOR_INFO}SWAP_USAGE    |${COLOR_VALUE} %s\n" "${SWAP_USAGE_PERCENT} (${SWAP_USAGE} MB of ${SWAP_TOTAL} MB)"
-printf " ${COLOR_INFO}DISK USAGE    |${COLOR_VALUE} %s\n" "${DISK_USAGE_PERCENT} (${DISK_USAGE} of ${DISK_TOTAL})"
-printf " ${COLOR_DEFAULT}"
-printf "==============================================\n"
-printf " ${COLOR_INFO}IP ADDRESS    |${COLOR_VALUE} %s\n" "${IP_ADDRESS}"
+printf " ${COLOR_HEAD}${SYSTEM_NAME}\n\n"
+printf " ${COLOR_HEADLINE}SYSTEM ${COLOR_DEFAULT}=======================================================================\n"
+printf " ${COLOR_INFO}DISTRIBUTION ${COLOR_DEFAULT}|${COLOR_VALUE} %s\n" "${DISTRIBUTION_NAME} ${DISTRIBUTION_VERSION} (${DISTRIBUTION_CODENAME})"
+printf " ${COLOR_INFO}CPU          ${COLOR_DEFAULT}|${COLOR_VALUE} %s\n" "${CPU_MODEL}- ${CPU_CORES} cores @ ${CPU_SPEED} MHz"
+printf " ${COLOR_INFO}DATE         ${COLOR_DEFAULT}|${COLOR_VALUE} %s\n" "${DATE}"
+printf " ${COLOR_INFO}TIMEZONE     ${COLOR_DEFAULT}|${COLOR_VALUE} %s\n" "${TIMEZONE}"
+printf " ${COLOR_INFO}UPTIME       ${COLOR_DEFAULT}|${COLOR_VALUE} %s\n" "${UPTIME}"
+printf " ${COLOR_INFO}PROCESSES    ${COLOR_DEFAULT}|${COLOR_VALUE} %s\n" "${PROCESSES_RUNNING} (running)"
+printf " ${COLOR_HEADLINE}USAGE ${COLOR_DEFAULT}========================================================================\n"
+printf " ${COLOR_INFO}CPU          ${COLOR_DEFAULT}|${COLOR_VALUE} %s\n" "${CPU_USAGE}% (${CPU_LOAD_AVG})"
+printf " ${COLOR_INFO}MEMORY       ${COLOR_DEFAULT}|${COLOR_VALUE} %s\n" "${MEMORY_USAGE_PERCENT}% (${MEMORY_USAGE} MB of ${MEMORY_TOTAL} MB)"
+printf " ${COLOR_INFO}SWAP         ${COLOR_DEFAULT}|${COLOR_VALUE} %s\n" "${SWAP_USAGE_PERCENT} (${SWAP_USAGE} MB of ${SWAP_TOTAL} MB)"
+printf " ${COLOR_INFO}DISK         ${COLOR_DEFAULT}|${COLOR_VALUE} %s\n" "${DISK_USAGE_PERCENT} (${DISK_USAGE} of ${DISK_TOTAL})"
+printf " ${COLOR_HEADLINE}NETWORK ${COLOR_DEFAULT}======================================================================\n"
+printf " ${COLOR_INFO}IP ADDRESS   ${COLOR_DEFAULT}|${COLOR_VALUE} %s\n" "${IP_ADDRESS}"
 printf " ${COLOR_DEFAULT}"
 printf "\n"
